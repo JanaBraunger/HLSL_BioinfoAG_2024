7a9420de96125f56a985e60a5715c2eeb967f205
schwarlu1691995 <52662097+schwarlu1691995@users.noreply.github.com>
2025-01-18 17:25:56 +0100

    final Proteome EDA

--- R_Skripte/EDA.R
+++ R_Skripte/EDA.R
@@ -5,7 +5,7 @@
 # 
 # 
 # In der "exploratory data analysis (EDA)" Phase versuchen wir erstmal uns einen generellen Überblick über die Daten zu verschaffen.
-# Wie viele Gene werden exprimiert?
+#   Wie viele Proteine wurden gemessen?
 #   Wie viele Proben wurden gemessen?
 #   Welche Vergleiche könnten interessant sein?
 #   Welche Proteine werden besonders stark/schwach exprimiert?
@@ -18,7 +18,9 @@
 # - Heatmaps
 # - volcano plot
 # 
-#   Außerdem benötigen wir einfache statistische Tests:
+#   Außerdem benötigen wir einfache statistische Mittel:
+#   - Mittelwert
+#   - Standardabweichung
 #   - T-Test
 #   
 
@@ -28,6 +30,8 @@
 # Datensätze einlesen:
 # verschiedene Lungenkrebs-Zelllinien wurden gemessen (RNAseq und Protein)
 # Wir fokussieren uns vorerst nur auf die Protein-Daten.
+# 
+#AUFGABE: Lade den Datensatz von github herunter, ändere den Pfad und lies den Datensatz ein.
 protein_daten = read.csv("K:/Ergebnisse/LS_testing/1_PhD/HLSL/transcriptome_proteome_dataset/lung_cell_lines_proteome.csv", row.names = 1)
 
 
@@ -39,20 +43,14 @@ head(protein_daten)
 dim(protein_daten)
 
 #Beschreibung der Zell-Linien
+##AUFGABE: Lade die Metadaten von github herunter, ändere den Pfad und lies den Datensatz ein.
+
 metadata = read.csv("K:/Ergebnisse/LS_testing/1_PhD/HLSL/transcriptome_proteome_dataset/lung_cell_lines_description.csv", row.names = 1)
 head(metadata)
 dim(metadata)
 # Welche Informationen haben wir zu den Zellen?
 
 
-#Stimmen die Namen überein?
-#Die %in% und table() Funktionen
-colnames(protein_daten)
-metadata$model_name
-
-
-colnames(protein_daten) %in% metadata$model_name
-table(colnames(protein_daten) %in% metadata$model_name)
 
 
 #Welche Organe sind abgedeckt? Die unique() Funktion
@@ -63,40 +61,33 @@ unique(metadata$tissue) #nur Lunge
 # Die table() Funktion
 table(metadata$tissue_status)
 #81x metastasis, 79x Tumor
-table(metadata$cancer_type)
-#84 x NSCLC
-#57 x SCLC
-# Es gibt verschiedene Lungenkrebs-Arten
 
-table(metadata$gender)
-table(metadata$ethnicity)
-table(metadata$smoking_status)
+# AUFGABE: Wende die unique() und table() Funktionen auf weitere Spalten an, die dich interessieren.
+
+
 
 # Histogramme:
 # Histogramme zeigen die Verteilung von Daten, d.h. welche Werte wie oft vorkommen
 # Nur für numerische Daten geeignet!
 hist(metadata$age_at_sampling)
 
+
 # verschiedene Daten-Typen (Klassen)
 # character (Text), numeric (Zahlen), logical (True/False)
 class(metadata$age_at_sampling)
-class(protein_daten$A427)
+
 
 hist(as.numeric(metadata$age_at_sampling))
 
-
-# 
-#Wie viele Proteine wurden in jeder Probe gemessen?
-# Funktionen: !is.na(), sum(), apply()
-# barplot() und boxplot()
+# AUFGABE: Wie könntest du Verteilung von nicht-numerischen Daten darstellen? z.B. von metadata$ethnicity
+# Tipp: table() + barplot()
 
 
+# Boxplots sind auch hilfreich, um die Verteilung von Daten anzuschauen und zu vergleichen:
+boxplot(as.numeric(metadata$age_at_sampling))
 
-
-barplot(apply(rna_seq, 2, function(x) sum(!is.na(x))))
-boxplot(apply(rna_seq, 2, function(x) sum(!is.na(x))))
-
-
+# AUFGABE: Wie könnten wir die Altersverteilung nicht von allen Lungenkrebs-Fällen, sondern nur NSCLC, darstellen?
+# TEIL2: Wie könnten wir die ALtersverteilung von NSCLC und SCLC nebeneinander vergleichen?
 
 
 # Welche Fragestellungen ergeben sich aus der EDA?
@@ -109,37 +100,33 @@ boxplot(apply(rna_seq, 2, function(x) sum(!is.na(x))))
 # 
 #install.packages("pheatmap")
 library(pheatmap)
-pheatmap(na.omit(protein_daten))
+pheatmap(na.omit(protein_daten), show_rownames = F, scale = "row")
+
+# Bessere Farben:
 
 #Wir können die columns mit hilfreichen Informationen beschriften:
 #Dazu nutzen wir die metadaten
 rownames(metadata) = metadata$model_name
 metadata$age_at_sampling = as.numeric(metadata$age_at_sampling)
 
-pheatmap(na.omit(protein_daten), scale = "row", show_rownames = F, show_colnames = F,
-         color = colorRampPalette(c("blue", "white", "red"))(100), breaks = seq(-5,5,10/100),
-         annotation_col = metadata[,c("cancer_type"), drop = F])
 
-# Falls die Farben nicht ideal sind:
+# Auch von diesen Beschriftungen können wir die Farben anpassen:
 unique(metadata$cancer_type)
 bessere_farben = list(cancer_type = c())
 
-pheatmap(na.omit(protein_daten), scale = "row", show_rownames = F, show_colnames = F,
-         color = colorRampPalette(c("blue", "white", "red"))(100), breaks = seq(-5,5,10/100),
-         annotation_col = metadata[,c("cancer_type"), drop = F], annotation_colors = bessere_farben)
-
-# Wie könnten wir mehrere Informationen gleichzeitig beschriften?
 
 
 # Statistik:Welche Gene sind unterschiedlich stark exprimiert?
 # z.B. SCLC vs. NSCLC
 # 
 # Wir wollen den "fold-change" und den "p-value" für jedes Gen herausfinden
-# --> Wie viel mal mehr/weniger ist jedes Gen exprimiert und ist dies signifikant?
+# --> Wie viel mal mehr/weniger ist jedes Gen exprimiert und ist dieser Unterschied signifikant?
 
 rownames(metadata) # Das sind alle Namen der Zelllinien
-head(protein_daten[,c("ABC1","DMS114")])
-head(protein_daten[,rownames(metadata)[1:4]])
+
+# Wiederholung: Auf bestimmte Teile einer Tabelle zugreifen.
+
+
 
 #wir wollen nur Zelllinien behalten, die entweder SCLC oder NSCLC sind:
 rownames(metadata)[metadata$cancer_type == "Small Cell Lung Carcinoma"]
@@ -154,31 +141,77 @@ pheatmap(na.omit(protein_sclc_nsclc), scale = "row", show_rownames = F, show_col
 
 
 # t.test() Funktion:
-#
+# Einfaches Beispiel:
 
-# fold-change = mean(group1)/mean(group2)
-# 1. Berechne für jedes Gen den Mittelwert in Gruppe 1 (SCLC)
-# mean(), apply()
-# 2. Berechne für jedes Gen den Mittelwert in Gruppe 2 (NSCLC)
-# 3. Teile die beiden Mittelwerte für jedes Gen durcheinander
-# mean1-mean2
+# Erklärung apply():
+
+# Jetzt wollen wir die t.test() Funktion auf alle Reihen unseres Datensatzes anwenden
+# Dabei soll SCLC gegen NSCLC getestet werden
+# In welchen Spalten sind die SCLC/NSCLC Zellen?
+length(rownames(metadata)[metadata$cancer_type == "Small Cell Lung Carcinoma"])
+# In den ersten 57 Spalten sind SCLC
+SCLC_spalten = c(1:57)
+length(rownames(metadata)[metadata$cancer_type == "Non-Small Cell Lung Carcinoma"])
+# In den nächsten 84 Spalten sind NSCLC
+NSCLC_spalten = c(58:(57+84))
+
+protein_sclc_nsclc$p_Wert = apply(protein_sclc_nsclc, 1, function(x) t.test(x[SCLC_spalten], x[NSCLC_spalten])$p.value)
+# Der T-Test kann nicht mit zu vielen fehlenden Werten umgehen.
+
+protein_sclc_nsclc = na.omit(protein_sclc_nsclc)
+#Jetzt müsste es Funktionieren
+
+# AUFGABE: Stelle die Verteilung der p-Werte dar!
+
+
+# fold-change = mean(SCLC)-mean(NSCLC)
+# AUFGABE:
+# 1. Berechne für jedes Protein den Mittelwert in Gruppe 1 (SCLC_spalten)
+# Tipps: mean(), apply()
+# 2. Berechne für jedes Gen den Mittelwert in Gruppe 2 (NSCLC_spalten)
+# 3. Ziehe jetzt die beiden Mittelwerte für jedes Protein voneinander ab
+# 
+protein_sclc_nsclc$FC = protein_sclc_nsclc$SCLC_mean - protein_sclc_nsclc$NSCLC_mean
+
 # Wie sieht die Verteilung dieser "fold-changes" aus?
 
+
 # Volcano Plot
+plot(protein_sclc_nsclc$FC, -log10(protein_sclc_nsclc$p_Wert))
 
-# Gene Set Enrichment
+# interessante Proteine beschriften:
 
+top10up = head(rownames(protein_sclc_nsclc)[order(protein_sclc_nsclc$FC, decreasing = T)],10)
+text(protein_sclc_nsclc[top10up, "FC"], -log10(protein_sclc_nsclc[top10up, "p_Wert"]), labels = top10up)
+
+
+# AUFGABE: Suche dir 2 Proteine aus und beschrifte diese im Plot.
+
+# Gene set enrichment:
+#install.packages("BiocManager")
+#BiocManager::install("ReactomePA")
+#install.packages("org.Hs.eg.db")
+library(ReactomePA)
+library(org.Hs.eg.db)
+
+gene_list = protein_sclc_nsclc$FC
+names(gene_list) = mapIds(org.Hs.eg.db, rownames(protein_sclc_nsclc), 'ENTREZID', 'SYMBOL')
+gene_list_sorted = gene_list[order(gene_list, decreasing = T)]
+
+
+
+GSEA_Ergebnis = gsePathway(gene_list_sorted, 
+                           pvalueCutoff = 0.05)
+
+dotplot(GSEA_Ergebnis, showCategory=20, x = "NES", color = "p.adjust", title = "Unterschiede zwischen SCLC und NSCLC")
 
 ## TRANSKRIPTOM:
-# Sehen wir ähnliche Unterschiede im Proteom?
+# Sehen wir ähnliche Unterschiede in den Transkriptom-Daten?
 # Wie sehr korrelieren Transkriptom und Proteom?
-# Gibt es Gene, die zwar stark exprimiert, aber anscheinend nicht translatiert werden? Oder andersherum?
-
-
-
 
 #RNA-seq Daten: Lungenkrebs-Zelllinien
 rna_seq = read.csv("K:/Ergebnisse/LS_testing/1_PhD/HLSL/transcriptome_proteome_dataset/lung_cell_lines_RNA_seq.csv", row.names = 1)
 
+# 
 
 
